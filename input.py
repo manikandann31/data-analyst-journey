ac839ed2c0375f574778d3a1216c34216b41c3b2
manikandann31 <manikandanpro31@gmail.com>
2025-11-10 19:17:51 +0530

    new example added

--- input.py
+++ input.py
@@ -1,10 +1,21 @@
 #method 1
 a=int(input("enter the value: "))
-#method2
 import sys
-full_name=sys.argv[1]
-last_name=sys.argv[2]
-email=full_name.lower().replace(" ",".")+last_name.lower()+ "@company.com"
-print("\n---your profile---")
-print("Full Name =",full_name + last_name)
-print("Generated Email =",email)
+
+# Method 2 - using command line arguments
+full_name = sys.argv[1]
+last_name = sys.argv[2]
+email = full_name.lower().replace(" ", ".") + "." + last_name.lower() + "@company.com"
+
+print("\n--- Your Profile ---")
+print("Full Name =", full_name, last_name)
+print("Generated Email =", email)
+
+#another example:
+
+A=int(input("Enter the value to display:"))
+print("The Entered value is :",A)
+ 
+name=sys.argv[3]
+print(name)
+
