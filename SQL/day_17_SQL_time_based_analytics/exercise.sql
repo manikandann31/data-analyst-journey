d8205c8255e8a6ea2e457b026ea7dc171648603d
Manikandann31 <manikandanpro31@gmail.com>
2026-01-27 20:17:39 +0530

    day 17 time based analysis workout

--- SQL/day_17_SQL_time_based_analytics/exercise.sql
+++ SQL/day_17_SQL_time_based_analytics/exercise.sql
@@ -5,10 +5,11 @@ select
 from enrollments
 group by enroll_month
 order by enroll_month;
+
 --MoM ANALYSIS 
 with monthly_data as (
 SELECT 
-    strftime('%Y-%m',e.enroll_date) as enroll_month,
+    strftime('%Y-%m',e.enroll_date) as enroll_month,  --STRFTIME is a function used in sqlite to extract month and year from the enroll date from the enrollments table 
     COUNT(*) AS monthly_enrollments
 FROM enrollments e
 JOIN courses c 
@@ -20,7 +21,7 @@ SELECT
     enroll_month,
     monthly_enrollments,
     lag(monthly_enrollments)over(
-        order by enroll_month) AS previous_month,
+        order by enroll_month) AS previous_month,               --growth is not only a total its a copmparison between previous count and current count so we have to subract the current count with the previous count to get the growth by month 
     monthly_enrollments-lag(monthly_enrollments)over(
         order by enroll_month) AS growth
 from monthly_data
